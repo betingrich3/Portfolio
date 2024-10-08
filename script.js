50ef4c50e523cfc9067eef06e917d2e14094918d
Marisel <shenapam3@gmail.com>
2024-10-08 13:36:27 +0300

    Marisel

---
+++ script.js
@@ -0,0 +1,10 @@
+// You can add JavaScript functionality later, such as scroll effects, interactive form, etc.
+// Here's a sample scroll smooth effect
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
